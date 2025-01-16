df35ac061bf4b3fe1fc21387d1c4b746bb662b8d
hameelhusnain <husnainhameel3@gmail.com>
2025-01-17 01:08:28 +0500

    Add HeroContent component with animation effects and layout styles

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+const HeroContent = () => {
+  return (
+    <motion.div
+    initial="hidden"
+    animate="visible"
+    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
+    >
+        <div className='flex flex-col h-full w-full gap-5 justify-center m-auto text-share'>
+            <motion.div
+            variants={slideInFromTop}
+            className='welcome-box py-[15px] px-[4px] border border-[#7042f88b] capacity-[0.9]'
+            >
+
+            </motion.div>
+
+        </div>
+
+    </motion.div>
+  )
+}
+
+export default HeroContent
