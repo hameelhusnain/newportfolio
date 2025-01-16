596f1a64886fc812d2475f3ef19e4f80ebf202e0
hameelhusnain <husnainhameel3@gmail.com>
2025-01-17 01:39:08 +0500

    Add custom styles for cursive font and welcome text, and integrate HeroContent component in Hero

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent'
+
 
 export const Hero = () => {
   return (
@@ -10,8 +12,8 @@ export const Hero = () => {
         className='rotate-180 absolute top[-340px] left-0 z-[1] w-full h-full object-cover'
         >
             <source src='/blackhole.webm' type='video/webm'/>
-
         </video>
+        <HeroContent/>
 
     </div>
   )
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react'
 import { motion } from 'framer-motion'
 import { slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from "@heroicons/react/24/solid";
 const HeroContent = () => {
   return (
     <motion.div
@@ -13,8 +15,8 @@ const HeroContent = () => {
             variants={slideInFromTop}
             className='welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
             >
-              {/* <SparklesIcons className='text-[#b49bff] mr-[10px] h-5 w-5'/> */}
-              <h1></h1>
+              <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
+              <h1 className='welcome-text text-[8px]'>Fullstack Developer Portfolio</h1>
 
             </motion.div>
         </div>
