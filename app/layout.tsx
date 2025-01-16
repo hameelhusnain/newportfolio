1a7c701536939e5546f72c562eba097dc349cf2a
hameelhusnain <husnainhameel3@gmail.com>
2025-01-17 00:33:38 +0500

    Update layout styles to enhance background and scrolling behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
         {children}
       </body>
