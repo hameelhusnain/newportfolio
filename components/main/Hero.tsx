9c12a394b80f3020f3973ba7eef379fb4625de9d
hameelhusnain <husnainhameel3@gmail.com>
2025-01-18 00:58:34 +0500

    Fix video position in Hero component for improved layout

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         autoPlay
         loop
         muted
-        className='rotate-180 absolute top[-340px] left-0 z-[1] w-full h-full object-cover'
+        className='rotate-180 absolute top-[-180px] left-0 z-[1] w-full h-full object-cover'
         >
             <source src='/blackhole.webm' type='video/webm'/>
         </video>
