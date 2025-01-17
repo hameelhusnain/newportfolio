ea64df594694bb99da64a615542846257136a197
hameelhusnain <husnainhameel3@gmail.com>
2025-01-18 01:02:20 +0500

    commit

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -26,7 +26,7 @@ const HeroContent = () => {
             className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
             >
               Providing
-              <span className='text-transparent bg-clip-text bg-gradint-to-r from-purple-500 to-cyan-500'> the best</span>
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best</span>
               project experience
             </motion.div>
             <motion.p
