94cb64219d7f1e1b22d1398bce5c858e78c162ec
hameelhusnain <husnainhameel3@gmail.com>
2025-01-18 00:10:47 +0500

    Fix layout and improve welcome box opacity in HeroContent component

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -10,13 +10,15 @@ const HeroContent = () => {
     animate="visible"
     className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
-        <div className='flex flex-col h-full w-full gap-5 justify-center m-auto text-share'>
+        <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-share'>
             <motion.div
             variants={slideInFromTop}
-            className='welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
+            className='welcome-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[2.0]'
             >
-              <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
-              <h1 className='welcome-text text-[8px]'>Fullstack Developer Portfolio</h1>
+              <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"/>
+              <h1 className='welcome-text text-[13px]'>
+                Fullstack Developer Portfolio
+              </h1>
 
             </motion.div>
         </div>
