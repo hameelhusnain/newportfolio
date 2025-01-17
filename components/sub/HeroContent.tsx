fd70f98cac7dbc0e3ea07439535158c57bf40c13
hameelhusnain <husnainhameel3@gmail.com>
2025-01-18 00:37:24 +0500

    Enhance HeroContent component with additional animations and correct class names for improved styling

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { motion } from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from "@heroicons/react/24/solid";
 const HeroContent = () => {
   return (
@@ -13,14 +13,26 @@ const HeroContent = () => {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-share'>
             <motion.div
             variants={slideInFromTop}
-            className='welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[2.0]'
+            className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[2.0]'
             >
               <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"/>
-              <h1 className='welcome-texthite text-w text-[13px]'>
+              <h1 className='Welcome-text text-w text-[13px]'>
                 Fullstack Developer Portfolio
               </h1>
-
             </motion.div>
+            <motion.div
+            variants={slideInFromLeft(0.5)}
+            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+            >
+              Providing
+              <span className='text-transparent bg-clip-text bg-gradint-to-r from-purple-500 to-cyan-500'> the best</span>
+              project experience
+            </motion.div>
+            <motion.p
+              variants={slideInFromLeft(0.8)}
+              className='text-lg text-gray-400 my-5 max-w-[600px]'
+            >
+            </motion.p>
         </div>
     </motion.div>
   )
