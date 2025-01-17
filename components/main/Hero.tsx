92a4512b7dc948a4fec27173527433320ba3a3c5
hameelhusnain <husnainhameel3@gmail.com>
2025-01-18 01:05:51 +0500

    Refactor Hero component for improved readability and layout consistency; add span for better text structure in HeroContent

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import HeroContent from '../sub/HeroContent'
+import React from "react";
+import HeroContent from "../sub/HeroContent";
 
-
-export const Hero = () => {
+const Hero = () => {
   return (
-    <div className=' relative flex flex-col h-full w-full'>
-        <video
+    <div className="relative flex flex-col h-full w-full" id="about-me">
+      <video
         autoPlay
-        loop
         muted
-        className='rotate-180 absolute top-[-180px] left-0 z-[1] w-full h-full object-cover'
-        >
-            <source src='/blackhole.webm' type='video/webm'/>
-        </video>
-        <HeroContent/>
-
+        loop
+        className="rotate-180 absolute top-[-180px]  h-full w-full left-0 z-[1] object-cover "
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
+      <HeroContent />
     </div>
-  )
-}
-export default Hero
+  );
+};
+
+export default Hero;
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -25,9 +25,11 @@ const HeroContent = () => {
             variants={slideInFromLeft(0.5)}
             className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
             >
+              <span>
               Providing
               <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best</span>
               project experience
+              </span>
             </motion.div>
             <motion.p
               variants={slideInFromLeft(0.8)}
