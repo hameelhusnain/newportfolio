878f0b8c043a7d39c072acca4ed6771ec9b260c6
hameelhusnain <husnainhameel3@gmail.com>
2025-01-19 23:49:41 +0500

    Refactor StarBackground component to include rotation and use random star generation

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,14 +1,28 @@
-'use client'
+'use client';
 
 
 
-import React, {useState, useRef, Suspense} from 'react'
-import {Canvas, useFrame} from '@react-three/fiber' 
-import {Points, PointMaterial, Preload} from '@react-three/drei'
-import * as random from 'maath/random/dist/maath-random.esm'
-const StarBackground = () => {
+import React, {useState, useRef, Suspense} from 'react';
+import {Canvas, useFrame} from '@react-three/fiber';
+import {Points, PointMaterial, Preload} from '@react-three/drei';
+// @ts-ignore
+import * as random from 'maath/random/dist/maath-random.esm';
+import { Group } from 'three';
+
+const StarBackground = (props: any) => {
+    const ref:  any = useRef();
+    const [sphere] = useState(() => 
+    random.inSphere(new Float32Array(5000), {radius: 1.2})
+);
+
+useFrame((state, delta) => {
+    ref.current.rotation.y -= delta / 10;
+    ref.current.rotation.x -= delta / 15;
+})
   return (
-    <div>StarBackground</div>
+    <Points positions={sphere} stride={3} ref={ref}>
+        <PointMaterial transparent color="#ffffff" size={0.005} sizeAttenuation={true} depthWrite={false} />
+    </Points>
   )
 }
 
