9ae6a2cd7e71ef518102773018d7de2e852384c6
hameelhusnain <husnainhameel3@gmail.com>
2025-01-19 23:20:16 +0500

    Fix font-weight class in HeroContent for correct text styling

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -23,7 +23,7 @@ const HeroContent = () => {
             </motion.div>
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
             >
               <span>
               Providing
