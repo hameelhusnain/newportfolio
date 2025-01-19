f8aee44c7755aa57fa81cda8b7bfc47eccc75d88
hameelhusnain <husnainhameel3@gmail.com>
2025-01-19 23:28:18 +0500

    Add StarBackground component for starry background effect

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+
+import React, {useState, useRef, Suspense} from 'react'
+import {Canvas, useFrame} from '@react-three/fiber' 
+
+const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground
