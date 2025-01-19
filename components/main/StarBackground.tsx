d4cc21fa59797541a2ab6295366696ea1232ed06
hameelhusnain <husnainhameel3@gmail.com>
2025-01-19 23:29:46 +0500

    Import necessary components from @react-three/drei in StarBackground for enhanced functionality

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -4,7 +4,7 @@
 
 import React, {useState, useRef, Suspense} from 'react'
 import {Canvas, useFrame} from '@react-three/fiber' 
-
+import {Points, PointMaterial, Preload} from '@react-three/drei'
 const StarBackground = () => {
   return (
     <div>StarBackground</div>
