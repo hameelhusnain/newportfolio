da1c290d30680bfa9e7a241c53afcfc049a1dd36
hameelhusnain <husnainhameel3@gmail.com>
2025-01-21 20:37:51 +0500

    Refactor StarBackground component to use GroupProps for improved type safety; update tsconfig for better structure and readability; add launch configuration for ESLint in VSCode

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -5,9 +5,11 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
+import { GroupProps } from '@react-three/fiber';
+
 
 const StarBackground = (props: any) => {
-  const ref: any = useRef(null);
+  const ref: any = useRef();
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
@@ -19,7 +21,7 @@ const StarBackground = (props: any) => {
 
 
   return (
-    <group rotation={[0.0, Math.PI / 4, 0]}>
+    <Group {...(props as GroupProps)} rotation={[0.0, Math.PI / 4, 0]}>
     <Points
         ref={ref}
         positions={sphere}
@@ -35,7 +37,7 @@ const StarBackground = (props: any) => {
             depthWrite={false}
         />
     </Points>
-</group>
+</Group>
 
   );
 };
