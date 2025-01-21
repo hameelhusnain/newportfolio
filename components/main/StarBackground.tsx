6b9ac0882b16125843e7602b88399073bf1d37f3
hameelhusnain <husnainhameel3@gmail.com>
2025-01-21 20:26:52 +0500

    Add React import to layout.tsx and remove unused import in StarBackground.tsx

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import group from "@react-three/fiber/dist/declarations/src/three-types";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
