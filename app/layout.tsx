c19c6202e2ab6113626fbdb2c9b7ebf63f802b6d
hameelhusnain <husnainhameel3@gmail.com>
2025-01-22 21:35:10 +0500

    Comment out StarBackground component and related code in layout and StarBackground files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import StarCanvas from "@/components/main/StarBackground";
+// import StarCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 
 const geistSans = Geist({
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarCanvas />
+        {/* <StarCanvas /> */}
         <Navbar />
         {children}
       </body>
--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,57 +1,57 @@
-"use client";
+// "use client";
 
-import React, { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as THREE from "three";
-// @ts-ignore
-import * as random from "maath/random/dist/maath-random.esm";
+// import React, { useState, useRef, Suspense } from "react";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// import { Points, PointMaterial, Preload } from "@react-three/drei";
+// import * as THREE from "three";
+// // @ts-ignore
+// import * as random from "maath/random/dist/maath-random.esm";
 
-const StarBackground = (props: any) => {
-  const ref: any = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
-  );
+// const StarBackground = (props: any) => {
+//   const ref: any = useRef();
+//   const [sphere] = useState(() =>
+//     random.inSphere(new Float32Array(5000), { radius: 1.2 })
+//   );
 
-  // Animate the rotation of the points
-  useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x -= delta / 10;
-      ref.current.rotation.y -= delta / 15;
-    }
-  });
+//   // Animate the rotation of the points
+//   useFrame((_, delta) => {
+//     if (ref.current) {
+//       ref.current.rotation.x -= delta / 10;
+//       ref.current.rotation.y -= delta / 15;
+//     }
+//   });
 
-  return (
-    // Use the `group` element from @react-three/fiber
-    <group rotation={[0, Math.PI / 4, 0]}>
-      <Points
-        ref={ref}
-        positions={sphere}
-        stride={3}
-        frustumCulled
-        {...props}
-      >
-        <PointMaterial
-          transparent
-          color="#fff"
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
-    </group>
-  );
-};
+//   return (
+//     // Use the `group` element from @react-three/fiber
+//     <group rotation={[0, Math.PI / 4, 0]}>
+//       <Points
+//         ref={ref}
+//         positions={sphere}
+//         stride={3}
+//         frustumCulled
+//         {...props}
+//       >
+//         <PointMaterial
+//           transparent
+//           color="#fff"
+//           size={0.002}
+//           sizeAttenuation={true}
+//           depthWrite={false}
+//         />
+//       </Points>
+//     </group>
+//   );
+// };
 
-const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 z-[20]">
-    <Canvas camera={{ position: [0, 0, 1] }}>
-      <Suspense fallback={null}>
-        <StarBackground />
-      </Suspense>
-      <Preload all />
-    </Canvas>
-  </div>
-);
+// const StarsCanvas = () => (
+//   <div className="w-full h-auto fixed inset-0 z-[20]">
+//     <Canvas camera={{ position: [0, 0, 1] }}>
+//       <Suspense fallback={null}>
+//         <StarBackground />
+//       </Suspense>
+//       <Preload all />
+//     </Canvas>
+//   </div>
+// );
 
-export default StarsCanvas;
+// export default StarsCanvas;
