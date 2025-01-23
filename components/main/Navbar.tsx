1cc440fc2ac18aa023cf493ff5f8fde98407dc1b
hameelhusnain <husnainhameel3@gmail.com>
2025-01-23 23:40:46 +0500

    Add logo image to Navbar for enhanced branding

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -5,6 +5,14 @@ const Navbar = () => {
     <div className='w-full h-[65px] fixed top-0 shadow-lg shadow=[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
       <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
         <a href='#about-me' className='h-auto w-auto flex flex-row item-center'>
+          <image 
+          src="/Navlogo.png"
+          alt='logo'
+          width={70}
+          height={70} 
+          className='curosor-pointer hover:animate-slowspin'
+          
+          />
 
         </a>
 
