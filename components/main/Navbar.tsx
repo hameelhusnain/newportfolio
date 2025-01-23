cd6d480984976f3bad1624b80e1198f94b301cd1
hameelhusnain <husnainhameel3@gmail.com>
2025-01-23 23:39:00 +0500

    Add anchor link to 'about-me' section in Navbar for improved navigation

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,6 +4,9 @@ const Navbar = () => {
   return (
     <div className='w-full h-[65px] fixed top-0 shadow-lg shadow=[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
       <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
+        <a href='#about-me' className='h-auto w-auto flex flex-row item-center'>
+
+        </a>
 
       </div>
     </div>
