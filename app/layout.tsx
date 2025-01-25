c40ac09a67f0719b7899dfc89dbfc07956a15c7f
hameelhusnain <husnainhameel3@gmail.com>
2025-01-25 23:03:04 +0500

    Refactor layout and styles: update font imports, add Footer component, and enhance StarBackground component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,31 @@
-import React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-// import StarCanvas from "@/components/main/StarBackground";
+import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Space Portfolio",
-  description: "This is my new portfolio",
+  description: "This is my portfolio",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        {/* <StarCanvas /> */}
+        <StarsCanvas />
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
